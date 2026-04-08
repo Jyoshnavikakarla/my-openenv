542f0248386cb4240ead78f651b2490711ce4f07
Jyoshnavikakarla <166583522+Jyoshnavikakarla@users.noreply.github.com>
2026-04-08 22:21:56 +0530

    Files added

--- server/app.py
+++ server/app.py
@@ -1,4 +1,10 @@
-from fastapi import FastAPI
-from inference import app as main_app
+from inference import app
 
-app = main_app
+
+def main():
+    import uvicorn
+    uvicorn.run("server.app:app", host="0.0.0.0", port=8080)
+
+
+if __name__ == "__main__":
+    main()
